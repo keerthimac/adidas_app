63e982403ab9db69aaf95b99d9b4cd9f01f10675
Darshana Keerthirathna <keerthi.mac@gmail.com>
2024-09-11 08:38:21 +0530

    commit msg

--- lib/screens/home_screen/homepage.dart
+++ lib/screens/home_screen/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:adidas_app/components/custom_text/custom_poppins_text.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatefulWidget {
@@ -9,6 +10,13 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  List<String> images = [
+    "https://images.unsplash.com/photo-1465453869711-7e174808ace9?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1620794341491-76be6eeb6946?q=80&w=1988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1557461761-c7c2b7a5fa97?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,6 +59,32 @@ class _HomepageState extends State<Homepage> {
                 color: Colors.grey.shade500,
                 fontSize: 15,
               ),
+              SizedBox(
+                height: 8,
+              ),
+              CarouselSlider(
+                options: CarouselOptions(height: 200.0),
+                items: images.map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 10,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                                image: NetworkImage(i), fit: BoxFit.cover)),
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+              CustomPoppinsText(
+                text: "Top Catogories",
+                fontWeight: FontWeight.w500,
+              )
             ],
           ),
         ),
